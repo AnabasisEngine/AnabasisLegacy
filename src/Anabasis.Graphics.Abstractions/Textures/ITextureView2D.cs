273ebb912dda1f8dc28228b12a860a92a7ba557b
Khitiara <robotbrainify@gmail.com>
2022-05-24 12:21:31 -0400

    texture sizes as vectors for convenience

--- src/Anabasis.Graphics.Abstractions/Textures/ITextureView2D.cs
+++ src/Anabasis.Graphics.Abstractions/Textures/ITextureView2D.cs
@@ -1,8 +1,11 @@
-﻿namespace Anabasis.Graphics.Abstractions.Textures;
+﻿using Silk.NET.Maths;
+
+namespace Anabasis.Graphics.Abstractions.Textures;
 
 public interface ITextureView2D
 {
     void UploadPixels(int level, int xOffset, uint xRange, int yOffset, uint yRange, ReadOnlySpan<Color> pixels);
     int Width { get; }
     int Height { get; }
+    public Vector2D<int> Size => new(Width, Height);
 }
--- src/Anabasis.Graphics.Abstractions/Textures/ITextureView3D.cs
+++ src/Anabasis.Graphics.Abstractions/Textures/ITextureView3D.cs
@@ -1,10 +1,14 @@
-﻿namespace Anabasis.Graphics.Abstractions.Textures;
+﻿using Silk.NET.Maths;
+
+namespace Anabasis.Graphics.Abstractions.Textures;
 
 public interface ITextureView3D
 {
     int Width { get; }
     int Height { get; }
     int Depth { get; }
+
+    public Vector3D<int> Size => new(Width, Height, Depth);
 
     void UploadPixels(int level, int xOffset, uint width, int yOffset, uint height, int zOffset, uint depth,
         ReadOnlySpan<Color> pixels);
